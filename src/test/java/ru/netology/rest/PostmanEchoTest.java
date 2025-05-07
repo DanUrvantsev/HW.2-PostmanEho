4271fca73749df52e0741d1d5b4e0ea743a12cc9
DanUrvantsev <laorant8@gmail.com>
2025-05-07 23:29:24 +0700

    postman eho

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+
+
+public class PostmanEchoTest {
+
+    @Test
+    void testPostRequestReturnsCorrectData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when().log().all()
+                .post("/post")
+                .then().log().all()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("some data"));
+    }
+}
